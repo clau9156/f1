30895911e57d753ef15bce1f98475c8a108942ce
marwane <marw0619@stud.kea.dk>
2021-03-23 19:29:03 +0100

    animations

---
+++ home.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.from("#configDivImg", { x: "-45vw" , scrollTrigger:{
+    trigger:"#triggerOne",
+    scrub:true , 
+    start:"top ",
+    end:"bottom" 
+    } });
+gsap.from("#textConfig", { x: "-45vw" , scrollTrigger:{
+    trigger:"#triggerOne",
+    scrub:true , 
+
+    start:"top", 
+    end:"bottom" ,
+    ease: "easeInOut"
+    } });
+
+gsap.from("#heritDivImg", { x: "45vw" , scrollTrigger:{
+    trigger:"#triggerTwo", 
+    scrub:true , 
+
+    start:"top 70%", 
+    end:"bottom 20%" } });
+gsap.from("#textHerit", { x: "45vw" , scrollTrigger:{
+    trigger:"#triggerTwo", 
+    scrub:true , 
+
+    start:"top 70%", 
+    end:"top  40%" } });
