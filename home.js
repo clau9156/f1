7a0503090446795be769a9a19df3fd71f6aee73f
clau9156 <clau9156@stud.kea.dk>
2021-03-24 18:42:12 +0100

    fixed scrolling page things and added sounds to home

--- home.js
+++ home.js
@@ -1,3 +1,14 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", carAnimation);
+
+function carAnimation() {
+    gsap.to(".carDrive", {duration: 3, x: "140vw", ease: "power3.in",  scrollTrigger:{trigger:".carDrive"}});
+    gsap.to("#textTitle", {duration: 3, x: "92vw", ease: "power3.in",  scrollTrigger:{trigger:"#textTitle"}});
+    document.querySelector("#audio").play();
+}
+
+
 gsap.registerPlugin(ScrollTrigger);
 gsap.from("#configDivImg", { x: "-45vw" , scrollTrigger:{
     trigger:"#triggerOne",
@@ -27,5 +38,4 @@ gsap.from("#textHerit", { x: "45vw" , scrollTrigger:{
     start:"top 70%", 
     end:"top  40%" } });
 
-gsap.to(".carDrive", {duration: 3, x: "140vw", ease: "power3.in",  scrollTrigger:{trigger:".carDrive"}});
-gsap.to("#textTitle", {duration: 3, x: "92vw", ease: "power3.in",  scrollTrigger:{trigger:"#textTitle"}});
+
